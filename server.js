2c00499c75572a2e844dc24c2a88b9c66541ffc1
Daniel Llewellyn <dllewellyn@redkitetechnologies.com>
2016-03-31 17:26:33 -0500

    added base data

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var AlexaAppServer = require( 'alexa-app-server' );
+
+var server = new AlexaAppServer( {
+	httpsEnabled: false,
+	port: process.env.PORT || 80
+} );
+
+server.start();
