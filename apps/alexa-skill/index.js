9e48f1b8c351202dfe725c2aca16a606cd86ae18
blseich <blseich@gmail.com>
2017-01-05 11:46:41 -0500

    finalize first test for skill. add gitignore

--- apps/alexa-skill/index.js
+++ apps/alexa-skill/index.js
@@ -2,7 +2,7 @@ module.change_code = 1;
 'use strict';
 
 var alexa = require( 'alexa-app' );
-var app = new alexa.app( 'test-skill' );
+var app = new alexa.app( 'alexa-skill' );
 
 
 app.launch( function( request, response ) {
